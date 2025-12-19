0dc42116d3ee097111afea2058d03600d209354e
Pranv <pranavsawant1011@gmail.com>
2025-12-19 12:56:48 +0530

    Performed oprtations

--- arithmetic_op.py
+++ arithmetic_op.py
@@ -1,4 +1,11 @@
 a = int(input("Enter a number : "))
 b = int(input("Enter another number : ")) 
 
-print("Numbers are " , a,"  ",b )
+print("Numbers are ", a,"  ",b)
+
+print('Addition : ',a+b)
+print('Subtraction : ',a-b)
+print('Multiplication : ',a*b)
+print('Division : ',a/b)
+print('Floor Division : ',a//b)
+print('Power : ',a**b)
