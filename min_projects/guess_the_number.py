318c2e5c5e9a2421155cfb71cc3bf8b19cdff82e
Pranv <pranavsawant1011@gmail.com>
2025-12-20 11:18:34 +0530

    added chances for guessing

--- min_projects/guess_the_number.py
+++ min_projects/guess_the_number.py
@@ -2,9 +2,14 @@ import random as r
 
 num = r.randint(1,20)
 print(f"{num}")
-guess = int(input("Guess the number between 1 - 20 :"))
+print("Guess the number between 1 - 20 in 5 chances  :")
 
-if guess == num:
-    print("You guessed it right ")
-else :
+for i in range (1,6):
+    guess=int(input("Enter number : "))
+    if guess == num:
+        print("You guessed it right ")
+        break
+    else :
+        print(f"{5-i} chances left ")
+else:
     print(f"Number was {num}")
