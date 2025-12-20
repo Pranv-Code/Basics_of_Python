93c0b9f6d702dd0ce9eb453d756b5914f589aebb
Pranv <pranavsawant1011@gmail.com>
2025-12-20 12:01:23 +0530

    log system added and modified formatting

--- min_projects/atm_system.py
+++ min_projects/atm_system.py
@@ -1,26 +1,37 @@
+# ATM System 
 def deposit(amt):
     global balance
     balance+=amt
-    print(f'\n{amt} deposited in account')
-    show_balance()
+    print(f'{amt} deposited in account')
+    bank_log.append(f'{amt} credited')
 
 def show_balance():
     print(f'Balance : {balance}')
+    bank_log.append('Balance Checked')
+
 
 def withdraw(amt):
     global balance
     if balance<amt:
-        print('Insuffecient Funds')
+        print('<Insuffecient Funds !!>')
+        bank_log.append('debit failed !')
+
     else:
         balance-=amt
-        print(f'\n{amt} withdrew from account')
-        show_balance()
-    
+        print(f'{amt} withdrew from account')
+        bank_log.append(f'{amt} debited')
+
+def show_logs():
+    for i in bank_log:
+        print('\n',i)
+
+
 balance = 0
-print('1. Deposit \t 2. Withdraw \t 3.Check Balance \t 4. Exit ')
+bank_log=[]
+print('1. Deposit \t 2. Withdraw \t 3.Check Balance \t 4. Logs \t 5. Exit ')
 ch = int(input('Enter Action Number : '))
 
-while ch != 4:
+while ch != 5:
     match ch:
         case 1: 
             depo = int(input('Enter amount to be diposited : ')) 
@@ -30,7 +41,10 @@ while ch != 4:
             withdraw(amt)
         case 3: 
             show_balance()
-        case 4: 
+        case 4:
+            show_logs()
+        case 5: 
+            print('Thank You!! Vist Again !')
             break
-    print('1. Deposit \t 2. Withdraw \t 3.Check Balance \t 4. Exit')
+    print('\n1. Deposit \t 2. Withdraw \t 3.Check Balance \t 4. Logs \t 5. Exit')
     ch = int(input('Enter Action Number : '))
