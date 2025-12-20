6e69a3aed2c0d041e93c21f99bca8708705967e0
Pranv <pranavsawant1011@gmail.com>
2025-12-20 22:41:54 +0530

    Updated conditions

--- min_projects/atm_system.py
+++ min_projects/atm_system.py
@@ -1,9 +1,12 @@
 # ATM System 
 def deposit(amt):
     global balance
-    balance+=amt
-    print(f'{amt} deposited in account')
-    bank_log.append(f'{amt} credited')
+    if amt >= 0:
+        balance+=amt
+        print(f'{amt} deposited in account')
+        bank_log.append(f'{amt} credited')
+    else : 
+        print("Negative amount cannot be deposited ")
 
 def show_balance():
     print(f'Balance : {balance}')
@@ -24,8 +27,6 @@ def withdraw(amt):
 def show_logs():
     for i in bank_log:
         print('\n',i)
-
-
 balance = 0
 bank_log=[]
 print('1. Deposit \t 2. Withdraw \t 3.Check Balance \t 4. Logs \t 5. Exit ')
@@ -34,8 +35,8 @@ ch = int(input('Enter Action Number : '))
 while ch != 5:
     match ch:
         case 1: 
-            depo = int(input('Enter amount to be diposited : ')) 
-            deposit(depo)
+            amt = int(input('Enter amount to be diposited : ')) 
+            deposit(amt)
         case 2: 
             amt = int(input('Enter amount to withdraw : ')) 
             withdraw(amt)
