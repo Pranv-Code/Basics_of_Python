e166eb8b6387932f400a477c2bb536442dc45268
Pranv <pranavsawant1011@gmail.com>
2025-12-20 10:08:58 +0530

    negative positive zero

---
+++ if_else/pos_neg.py
@@ -0,0 +1,11 @@
+# Find the number is positive negative or zero
+
+num1=int(input("Enter a number : "))
+
+if num1>0:
+    print(f"{num1} is Positive")
+elif num1<0:
+    print(f"{num1} is Negative")
+else:
+    print(f"Number is Zero")
+    
