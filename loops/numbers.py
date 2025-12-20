a5bf7b6cb270dddb4948a30b7d257ac6efd055af
Pranv <pranavsawant1011@gmail.com>
2025-12-20 10:48:58 +0530

    numbers using loops

---
+++ loops/numbers.py
@@ -0,0 +1,10 @@
+# print numbers from 1-10 
+print("Using for loop ")
+for i in range (1,11):
+    print(i,end=" ")
+i=1
+print("\nUsing while loop ")
+
+while i <= 10 :
+    print(i,end=" ")
+    i+=1
